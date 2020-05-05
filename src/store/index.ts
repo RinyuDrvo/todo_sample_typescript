7e25f14312ab7a7682290c388133636fc2803fbf
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-05 03:28:12 +0000

    fix store index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { createTodoListModule } from '@/store/TodoListStore';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+const store = new Vuex.Store({});
+
+// todoリストモジュールを動的に登録する
+store.registerModule('todos', createTodoListModule(store));
+
+export default store;
